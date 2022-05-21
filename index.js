edb0cbce5e425427ff818b3116a8df04ff0f212f
Zabih <mdzabihhaqqani@gmail.com>
2022-05-21 16:49:23 +0000

    add more questions

--- index.js
+++ index.js
@@ -1,56 +1,70 @@
 let readlineSync = require("readline-sync");
 
-let userName = readlineSync.question("What is your name ? ")
+// const chalk = require('chalk');
+
+//Taking input from the user//
+
+let userName = readlineSync.question("What is your name ?\n")
 
 let score = 0;
 
-console.log("Welcome ", userName ,"to the game on how well you know Zabih");
+console.log("Welcome ", userName, "to the game on how well you know Zabih\n");
 
 console.log("------------------------------------------------")
 
+// processing the data //
 
-function questions(question,answer) {
+function questions(question, answer) {
   let userAnswer = readlineSync.question(question);
 
-  if(userAnswer.toUpperCase() === answer.toUpperCase()) {
-    console.log("right answer");
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log("right answer\n");
     score = score + 1;
-    
+
   } else {
-    console.log("wrong!!!")
-     
+    console.log("wrong!!!\n")
+
   }
-  
+
   console.log("currrent score is ", score)
   console.log("------------------")
 }
 
 let allQuestions = [{
   question: "What is my favourite ice cream ",
-  answer:"Butterscotch"
+  answer: "Butterscotch"
 }, {
   question: "What is my favourite colour ",
-  answer:"blue"
+  answer: "blue"
 }, {
   question: "Where do I live ",
-  answer:"hyderabad"
- } ,{
-     question: "What is my favourite sport ",
-  answer:"Badminton"
-   }
+  answer: "hyderabad"
+}, {
+  question: "What is my favourite sport ",
+  answer: "Badminton"
+}, {
+  question: "What is my college name in short ",
+  answer: "MJCET"
+}, {
+  question: "What is my other name ",
+  answer: "faisal"
+}
 ];
 
-for (let i=0; i<allQuestions.length; i++ ){
+// giving the output //
+
+
+for (let i = 0; i < allQuestions.length; i++) {
   let currentQuestion = allQuestions[i];
-  questions(currentQuestion.question,currentQuestion.answer)
+  questions(currentQuestion.question, currentQuestion.answer)
 
 }
 
 console.log("final score = ", score)
-if(score === 4) {
- 
+if (score === 4) {
+
   console.log("YAY you got all of them right")
 
 } else {
-  console.log("You got",score,"of them right" )
+  console.log("You got", score, "of them right")
 }
